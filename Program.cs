eff71d52dde0f62697cd9988169ad4a36127f8bf
Keith <kayceecode@gmail.com>
2024-05-13 20:31:04 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCors();
+builder.Services.AddAuthentication().AddJwtBearer();
+builder.Services.AddAuthorization();
+
+var app = builder.Build();
+
+app.UseCors();
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapGet("/", () => "Hello World!");
+
+app.Run();
