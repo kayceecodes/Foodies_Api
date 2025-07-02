0eb9c2a8ea38208ddf1e0f12796c896c0e5ef57e
Keith <kayceecode@gmail.com>
2025-07-01 22:09:08 -0400

    added logout route in endpoints

--- Endpoints/AuthEndpoints.cs
+++ Endpoints/AuthEndpoints.cs
@@ -1,7 +1,7 @@
+using Microsoft.AspNetCore.Mvc;
 using AutoMapper;
 using foodies_api.Data;
 using foodies_api.Models.Dtos.Requests;
-using Microsoft.AspNetCore.Mvc;
 using foodies_api.Models;
 using foodies_api.Models.Entities;
 using foodies_api.Interfaces.Services;
@@ -32,6 +32,22 @@ public static class AuthEndpoints
         ([FromBody] RegisterRequest request, IAuthService service, AppDbContext context, IConfiguration config, IMapper mapper) =>
         {
             var result = await service.Register(request);
+
+            if (!result.IsSuccess)
+                return TypedResults.BadRequest(result);
+
+            return TypedResults.Ok(result);
+        })
+        .WithName("Register User")
+        .Accepts<string>("application/json")
+        .Produces<ApiResult<List<User>>>(StatusCodes.Status200OK)
+        .Produces(StatusCodes.Status400BadRequest)
+        .WithOpenApi();
+
+        app.MapPost("/api/logout", async Task<IResult>
+        ([FromBody] IAuthService service, HttpContext context) =>
+        {
+            var result = await service.Logout();
 
             if (!result.IsSuccess)
                 return TypedResults.BadRequest(result);
---
+++ Models/Dtos/Responses/LogoutResponse.cs
@@ -0,0 +1,7 @@
+namespace foodies_api.Models.Dtos.Responses;
+
+public class LogoutResponse
+{
+    public string Username { get; set; }
+    public string Email { get; set; }
+}
