dbb55f6b703b37f5b40d8972d3658beb705d5223
Keith <kayceecode@gmail.com>
2025-10-27 22:56:57 -0400

    edited profile mapping.

--- Models/Dtos/Responses/LoginResponse.cs
+++ Models/Dtos/Responses/LoginResponse.cs
@@ -5,7 +5,7 @@ public class LoginResponse
 {
     public string Token { get; set; }
     public string Username { get; set; }
-    public string FistName { get; set; }
+    public string FirstName { get; set; }
     public string LastName { get; set; }
     public string Zipcode { get; set; }
 }
